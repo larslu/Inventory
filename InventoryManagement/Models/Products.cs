ece5762c8f5da192e1f2f6898cbfc17f5f625c91
Lars Lundin <larslundin72@live.se>
2020-02-09 15:57:55 +0100

    Add project files.

---
+++ InventoryManagement/Models/Products.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace InventoryManagement.Models
+{
+    public partial class Products
+    {
+        public long ProductId { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public string Color { get; set; }
+        public decimal UnitPrice { get; set; }
+        public long AvailableQuantity { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime CratedDate { get; set; }
+    }
+}
